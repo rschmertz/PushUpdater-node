e081c68ee435f9cc32e2dd4652ad83bbbdb2238c
Bob Schmertz <rschmertz@yahoo.com>
2013-03-06 21:56:12 -0500

    Displaying point updates in index page

--- public/javascripts/test-client.js
+++ public/javascripts/test-client.js
@@ -1,12 +1,28 @@
 /* Deprecated: pure callback */
-function pointHandler(data) {
-    console.log('pointhandler called, id = %d', pointHandler._updaterUse.guid);
-};
+
+
+var pointHandler = {
+    update: function (update) {
+        console.log('pointhandler called, id = %d', this._updaterUse.guid);
+        //console.log('point value is' + data);
+        this.value(update.data);
+    }
+    , value: ko.observable(0)
+}
+
+ko.applyBindings(pointHandler, document.getElementById("pointupdater"))
+
+var eventCounter = 0;
 
 var eventHandler = {
     update: function (data) {
         console.log('eventhandler called, id = %d', this._updaterUse.guid);
+        this.list.push({
+            even: eventCounter % 2 == 0 ? true : false
+            , data: data
+        });
     }
+    , list: ko.observableArray()
 }
 
 require(["updater-client"], function(updater) {
